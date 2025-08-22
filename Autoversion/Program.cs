06928ec433df64807c1191625297beb269d4f71e
Andrii Pukhkalo <ANDP@simcorp.com>
2025-08-22 14:42:40 +0200

    041

--- Autoversion/Program.cs
+++ Autoversion/Program.cs
@@ -8,3 +8,4 @@ Console.WriteLine("Add feature 3 - commit 1");
 Console.WriteLine("release-0.3.0-fix-1");
 Console.WriteLine("hotfix1 in 030");
 Console.WriteLine("Add feature 4 - commit 1");
+Console.WriteLine("hotfix in support in 040");
